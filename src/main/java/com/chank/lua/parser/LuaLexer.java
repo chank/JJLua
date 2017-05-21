4d94a4c99d65b2610379752d51ccf6f0880cbb82
chank <fangliquan@qq.com>
2017-05-21 15:15:34 +0800

    Lua lexer.

--- src/main/java/com/chank/lua/parser/LuaLexer.java
+++ src/main/java/com/chank/lua/parser/LuaLexer.java
@@ -21,6 +21,8 @@ import com.chank.lua.LuaTValue;
 import com.chank.lua.util.ZIO;
 import com.chank.lua.util.ZIOUtil;
 
+import static com.chank.lua.util.ZIO.EOZ;
+
 /**
  * Created by chank on 2017/2/23.
  */
@@ -177,7 +179,7 @@ public final class LuaLexer {
             incLineNumber(ls);
         for (;;) {
             switch (ls.current) {
-                case ZIO.EOZ: {
+                case EOZ: {
                     break;
                 }
                 case ']': {
@@ -204,7 +206,7 @@ public final class LuaLexer {
 
     private static final void escCheck(LexState ls, boolean c, String msg) {
         if (!c) {
-            if (ls.current != ZIO.EOZ)
+            if (ls.current != EOZ)
                 saveAndNext(ls);
         }
     }
@@ -267,11 +269,11 @@ public final class LuaLexer {
         return r;
     }
 
-    private static final void readString(LexState ls, int del, SemInfo semInfo) {
+    private static final void readString(LexState ls, int del, SemInfo semInfo) throws Exception {
         saveAndNext(ls);
         while (ls.current != del) {
             switch (ls.current) {
-                case ZIO.EOZ:
+                case EOZ:
                     lexError(ls, "unfinished string", Reserved.TK_EOS);
                     break;
                 case '\n':
@@ -282,15 +284,173 @@ public final class LuaLexer {
                     int c;
                     saveAndNext(ls);
                     switch (ls.current) {
-                        case 'a':
-                            c = 'a';
+                        case 'a': // TODO
+                        case 'b':c = '\b'; next(ls); break;
+                        case 'f':c = '\f'; next(ls); break;
+                        case 'n': c = '\n'; next(ls); break;
+                        case 'r': c = '\r'; next(ls); break;
+                        case 't': c = '\t'; next(ls); break;
+                        case 'v': c = readHexaEsc(ls); next(ls); break;
+                        case 'x': c = readHexaEsc(ls); next(ls); break;
+                        case 'u': utf8Esc(ls); break;
+                        case '\n':case '\r':
+                            incLineNumber(ls); c = '\n'; save(ls, c);
+                        case '\\':case '\"':case '\'':
+                            c = ls.current; next(ls); break;
+                        case EOZ: break;
+                        case 'z': {
+                            next(ls);
+                        }
                     }
                 default:
             }
         }
+        saveAndNext(ls);
     }
 
     private static final void lexError(LexState ls, String msg, Reserved token) {
+    }
+
+    public static int llex(LexState ls, SemInfo semInfo) throws Exception {
+        for (;;) {
+            switch (ls.current) {
+                case '\n': case '\r': {
+                    incLineNumber(ls);
+                    break;
+                }
+                case ' ':case '\f':case '\t': {
+                    next(ls);
+                    break;
+                }
+                case '-': {
+                    next(ls);
+                    if (ls.current != '-')
+                        return '-';
+                    next(ls);
+                    if (ls.current == '[') {
+                        int sep = skipSep(ls);
+                        if (sep >= 0) {
+                            readLongString(ls, null, sep);
+                            break;
+                        }
+                    }
+                    while (!isCurrentNewline(ls) && ls.current != EOZ) {
+                        next(ls);
+                    }
+                    break;
+                }
+                case '[': {
+                    int sep = skipSep(ls);
+                    if (sep >= 0) {
+                        readLongString(ls, semInfo, sep);
+                        return Reserved.TK_STRING.getValue();
+                    } else if (sep != -1) {
+                        lexError(ls, "Invalid long string delimiter", Reserved.TK_STRING);
+                    }
+                    return '[';
+                }
+                case '=': {
+                    next(ls);
+                    if (checkNext1(ls, '=')) {
+                        return Reserved.TK_EQ.getValue();
+                    } else {
+                        return '=';
+                    }
+                }
+                case '<': {
+                    next(ls);
+                    if (checkNext1(ls, '=')) {
+                        return Reserved.TK_LE.getValue();
+                    } else if (checkNext1(ls, '<')) {
+                        return Reserved.TK_SHL.getValue();
+                    } else {
+                        return '<';
+                    }
+                }
+                case '>': {
+                    next(ls);
+                    if (checkNext1(ls, '=')) {
+                        return Reserved.TK_GE.getValue();
+                    } else if (checkNext1(ls, '>')) {
+                        return Reserved.TK_SHR.getValue();
+                    } else {
+                        return '>';
+                    }
+                }
+                case '/': {
+                    next(ls);
+                    if (checkNext1(ls, '/')) {
+                        return Reserved.TK_IDIV.getValue();
+                    } else {
+                        return '/';
+                    }
+                }
+                case '~': {
+                    next(ls);
+                    if (checkNext1(ls, '=')) {
+                        return Reserved.TK_NE.getValue();
+                    } else {
+                        return '~';
+                    }
+                }
+                case ':': {
+                    next(ls);
+                    if (checkNext1(ls, ':')) {
+                        return Reserved.TK_DBCOLON.getValue();
+                    } else {
+                        return ':';
+                    }
+                }
+                case '"':case '\'': {
+                    readString(ls, ls.current, semInfo);
+                    return Reserved.TK_STRING.getValue();
+                }
+                case '.': {
+                    saveAndNext(ls);
+                    if (checkNext1(ls, '.')) {
+                        if (checkNext1(ls, '.')) {
+                            return Reserved.TK_DOTS.getValue();
+                        } else {
+                            return Reserved.TK_CONCAT.getValue();
+                        }
+                    } else if (!isDigit(ls.current)) {
+                        return '.';
+                    } else {
+                        return readNumeral(ls, semInfo);
+                    }
+                }
+                case '0': case '1': case '2': case '3':case '4':
+                case '5': case '6': case '7': case '8': case '9': {
+                    return readNumeral(ls, semInfo);
+                }
+                case ZIO.EOZ: {
+                    return Reserved.TK_EOS.getValue();
+                }
+                default: {
+                    if (isAlpha(ls.current)) {
+                        do {
+                            saveAndNext(ls);
+                        } while (isAlphanumericNum(ls.current));
+                    }
+                }
+            }
+        }
+    }
+
+    private void luaXNext(LexState ls) throws Exception {
+        ls.lastLine = ls.lineNumber;
+        if (ls.lookahead.token != Reserved.TK_EOS.getValue()) {
+            ls.t = ls.lookahead;
+            ls.lookahead.token = Reserved.TK_EOS.getValue();
+        } else {
+            ls.t.token = llex(ls, ls.t.semInfo);
+        }
+    }
+
+    private int luaXLookahead(LexState ls) throws Exception {
+        assert (ls.lookahead.token == Reserved.TK_EOS.getValue());
+        ls.lookahead.token = llex(ls, ls.lookahead.semInfo);
+        return ls.lookahead.token;
     }
 
 }
